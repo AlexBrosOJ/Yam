7c95f6c3ca72c10a6d219dbfcbb4b9b2e782cfbb
AlexBrosOJ <alex.plastun@mail.ru>
2026-02-01 20:35:29 +0300

    New model
    93% model

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 # ---- Configuration ----
 SERVER_TIMEZONE = pytz.timezone('Europe/Moscow')
 CLEANUP_INTERVAL_HOURS = 5  # Удалять записи старше 1 часа
-THRESHOLD = 0.625  # Порог для кашля (можешь поменять)
+THRESHOLD = 0.63  # Порог для кашля (можешь поменять)
 
 def get_current_datetime():
     return datetime.now(SERVER_TIMEZONE).strftime("%Y-%m-%d %H:%M:%S")
